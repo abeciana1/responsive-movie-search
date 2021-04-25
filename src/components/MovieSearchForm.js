bac7b22406d11d5a65bc0bacd3bf5a3dd917dd7a
Alex <alex.beciana@gmail.com>
2021-04-25 16:42:53 -0400

    Created movie search form

---
+++ src/components/MovieSearchForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import '../css/movie-search-form.css'
+
+class MovieSearchForm extends React.Component {
+
+    state = {
+        searchTerm: ""
+    }
+
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <form>
+                    <input
+                        class="search-bar"
+                        type="text"
+                        name="searchTerm"
+                        placeholder="Type a movie title"
+                        value={this.state.searchTerm}
+                        onChange={this.changeHandler}
+                        />
+                </form>
+            </React.Fragment>
+        )
+    }
+}
+
+export default MovieSearchForm
